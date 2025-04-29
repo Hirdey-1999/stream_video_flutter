e861f3e4eeb5e7904cf1d30406b1774e87f4075f
Hirdey-1999 <hirdeymittal@gmail.com>
2025-04-29 11:07:32 +0530

    Inital Commit

---
+++ lib/src/call_controls/controls/default_control_options.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../../../stream_video_flutter.dart';
+
+/// Builds the default set of call control options.
+List<Widget> defaultCallControlOptions({
+  required Call call,
+  required CallParticipantState localParticipant,
+  VoidCallback? onLeaveCallTap,
+}) {
+  return [
+    ToggleSpeakerphoneOption(call: call),
+    ToggleCameraOption(call: call, localParticipant: localParticipant),
+    ToggleMicrophoneOption(call: call, localParticipant: localParticipant),
+    FlipCameraOption(call: call, localParticipant: localParticipant),
+  ];
+}
